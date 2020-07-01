05fa2ea776ebf7e44ec86e8a39641a1b78c41999
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-01 11:01:39 -0500

    esther

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,13 @@
 import React from "react";
 
-const Search = () => {
+const Search = (props) => {
+  console.log("POROPS", props)
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
-        onChange={() => {
-          console.log("Searching...");
-        }}
+        onChange={props.filterTransactions}
       />
       <i className="circular search link icon"></i>
     </div>
