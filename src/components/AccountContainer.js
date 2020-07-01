ebcfe223dfad133a29b019f7ed1b88917cb5d983
floresfreddy <floresfreddy.ultra@gmail.com>
2020-07-01 11:01:08 -0500

    kinda finished

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,65 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const url = `http://localhost:6001`
+
 class AccountContainer extends Component {
+  
+  state = {
+    transactions: [],
+    newT : {},
+    searchTerm : '',
+    filteredT: []
+  }
+
+  addTransaction = (e) => {
+    
+    const options = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(e)
+    }
+    fetch(`${url}/transactions`, options)
+      .then( this.setState({
+        transactions: [...this.state.transactions, e]
+      }))
+  }
+
+  searchTransaction = (search) => {
+      
+    var results =  this.state.transactions.filter(function(t) {
+      return t.description.includes(search);
+    });
+    console.log(results)
+    this.setState({
+      filteredT : results
+    })
+      
+  }
+
+  deleteTransaction = () => {
+
+  }
+  
+  componentDidMount(){
+    fetch(`${url}/transactions`)
+      .then(res => res.json())
+      .then(transactions => {
+        this.setState({
+          transactions: transactions,
+          filteredT: transactions
+        })
+      })
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searching = {this.searchTransaction} />
+        <AddTransactionForm adding = {this.addTransaction}/>
+        <TransactionsList transactions = {this.state.filteredT}/>
       </div>
     );
   }
