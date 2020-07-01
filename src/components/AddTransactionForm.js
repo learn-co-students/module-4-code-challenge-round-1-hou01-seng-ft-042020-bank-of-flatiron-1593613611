ed515cebb43f46ec25f8a05ae27cbb525d9e435f
devchea <rchea817@gmail.com>
2020-07-01 08:55:29 -0700

    done

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  state = {
+    inputDate: null,
+    inputDescription: null,
+    inputCategory: null,
+    inputAmount: null
+  }
+
   render() {
+    // console.log(this.state)
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={(e) => this.setState({ inputDate: e.target.value})}/>
+            <input type="text" name="description" placeholder="Description" onChange={(e) => this.setState({ inputDescription: e.target.value})}/>
+            <input type="text" name="category" placeholder="Category" onChange={(e) => this.setState({ inputCategory: e.target.value})}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={(e) => this.setState({ inputAmount: e.target.value})}
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" onClick={(e)=>this.props.handleSubmit(e, this.state)}>
             Add Transaction
           </button>
         </form>
